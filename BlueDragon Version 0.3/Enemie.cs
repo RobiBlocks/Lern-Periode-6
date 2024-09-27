9f30bf98e0bb619269458619b7de637572979a0a
RobiBlocks <142887221+RobiBlocks@users.noreply.github.com>
2024-09-27 16:13:48 +0200

    Add files via upload

---
+++ BlueDragon Version 0.3/Enemie.cs
@@ -0,0 +1,30 @@
+﻿using BlueDragon.Equipment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueDragon
+{
+    internal class Enemie
+    {
+        public string Name { get; }
+        public int Health { get; set; }
+        public int Speed { get; }
+        public Weapon Weapon { get; set; }
+        public Enemie(string name, int health, int speed, Weapon weapon)
+        {
+            Name = name;
+            Health = health;
+            Speed = speed;
+            Weapon = weapon;
+        }
+        public void attack(Hero opponent, Weapon weapon)
+        {
+            opponent.Health -= weapon.Damage;
+            weapon.Durability -= 1;
+        }
+    }
+}
